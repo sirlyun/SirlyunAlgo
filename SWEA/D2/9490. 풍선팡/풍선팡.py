d25e83da372ec8d1496daedbea9618d7da97e694
sirlyun <116432941+sirlyun@users.noreply.github.com>
2023-08-02 13:59:26 +0900

    [D2] Title: 풍선팡, Time: 318 ms, Memory: 63,036 KB -BaekjoonHub

---
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -0,0 +1,26 @@
+T = int(input())
+for t in range(1, T+1):
+    N, M = map(int, input().split())
+    ball_list = []
+    for n in range(N):
+        flower = list(map(int, input().split()))
+        ball_list.append(flower)
+
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+
+    max_pop = 0
+    for i in range(N):
+        for j in range(M):
+            ball = ball_list[i][j]
+            for k in range(4):
+                for l in range(1, ball_list[i][j]+1):
+                    di = i + dr[k]*l
+                    dj = j + dc[k]*l
+
+                    if 0 <= di < N and 0 <= dj < M:
+                        ball += ball_list[di][dj]
+            if max_pop < ball:
+                max_pop = ball
+
+    print(f'#{t} {max_pop}')
