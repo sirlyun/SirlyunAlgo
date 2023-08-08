173eef6776450c7b3267115add9c5c508b03c6fb
sirlyun <116432941+sirlyun@users.noreply.github.com>
2023-08-08 10:42:30 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 109 ms, Memory: 50,192 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -4,18 +4,13 @@ for _ in range(10):
     sent = input()
 
     result = 0
-    # for i in range(len(sent)-len(chk)):
-    #     j = 0
-    #     while j < len(chk):
-    #         if sent[i] != chk[j]:
-    #             break
-    #         j += 1
-    #         i += 1
-    #     else:
-    #         result += 1
-    # print(f'#{t} {result}')
-    for i in range(len(sent)):
-        if sent[i] == chk[0]:
-            if sent[i:i+len(chk)] == chk:
-                result += 1
+    for i in range(len(sent)-len(chk)+1):
+        j = 0
+        while j < len(chk):
+            if sent[i] != chk[j]:
+                break
+            j += 1
+            i += 1
+        else:
+            result += 1
     print(f'#{t} {result}')
