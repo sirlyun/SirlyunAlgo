ac511f855a27bf9e5c2781e4a25487c82e834fa3
sirlyun <116432941+sirlyun@users.noreply.github.com>
2023-09-01 20:18:34 +0900

    [Gold V] Title: A와 B 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,16 @@
+def dfs(t):
+    if t==S:
+        print(1)
+        exit()
+    if len(t)==0:
+        return 0
+    if t[-1]=='A': 
+        dfs(t[:-1])
+    if t[0]=='B': 
+        dfs(t[1:][::-1])
+
+S=list(input())
+T=list(input())
+
+dfs(T)
+print(0)
