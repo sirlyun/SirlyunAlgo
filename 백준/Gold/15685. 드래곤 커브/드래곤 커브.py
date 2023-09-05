6d898457bdb865094f9a5529caa6e040af0bce79
sirlyun <116432941+sirlyun@users.noreply.github.com>
2023-09-05 21:12:17 +0900

    [Gold IV] Title: 드래곤 커브, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -1,33 +1,28 @@
-n = int(input())
-
-graph = [[0] * 101 for _ in range(101)]
+N = int(input())
+map_list = [[0]*101 for _ in range(101)]
 dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 
-for i in range(n):
+for n in range(N):
+    y, x, d, g = map(int, input().split())
+    map_list[x][y] = 1
 
-    y, x, d, g = map(int, input().split(' '))
-    graph[x][y] = 1
 
-    # 커브 리스트 만들기
-    curve = [d]
-    for j in range(g):
-        for k in range(len(curve) - 1, -1, -1):
-            curve.append((curve[k] + 1) % 4)
+    chk = [d]
+    for i in range(g):
+        for j in range(len(chk)-1, -1, -1):
+            chk.append((chk[j]+1)%4)
 
-    # 드래곤 커브 만들기
-    for j in range(len(curve)):
-        x += dx[curve[j]]
-        y += dy[curve[j]]
-        if x < 0 or x >= 101 or y < 0 or y >= 101:
-            continue
+    for i in range(len(chk)):
+        x += dx[chk[i]]
+        y += dy[chk[i]]
+        if 0<=x<101 and 0<=y<101:
+            map_list[x][y] = 1
 
-        graph[x][y] = 1
-
-answer = 0
+result = 0
 for i in range(100):
     for j in range(100):
-        if graph[i][j] == 1 and graph[i + 1][j] == 1 and graph[i][j + 1] == 1 and graph[i + 1][j + 1] == 1:
-            answer += 1
+        if map_list[i][j] == 1 and map_list[i+1][j] == 1 and map_list[i][j+1] == 1 and map_list[i+1][j+1]:
+            result += 1
 
-print(answer)
+print(result)
