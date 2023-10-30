bc9878140aae49cd497ac61cbfd455e88996a54c
sirlyun <116432941+sirlyun@users.noreply.github.com>
2023-10-30 18:37:28 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 12.60 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,47 +1,55 @@
 class Solution {
    public String solution(String[] survey, int[] choices) {
-		String answer = "";
+		String[] og = {"RT", "CF", "JM", "AN"};
+        String[] rv = {"TR", "FC", "MJ", "NA"};
 
-		char[] person = { 'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N' }; // 성격 유형
-		int[] score = new int[8]; // 성격 유형 점수
+        int[][] chk = {
+                {0, 0}, {0, 0}, {0, 0}, {0, 0}
+        };
+        
+        String answer = "";
 
-		for (int i = 0; i < survey.length; i++) {
-			char da = '\0';
-			int s = choices[i]; // 선택지
-			if (s < 4) { // 비동의 관련 선택지
-				da = survey[i].charAt(0); // 첫 번째 캐릭터
-				if (s == 1) {
-					s = 3;
-				} else if (s == 3) {
-					s = 1;
-				}
-			} else if (s > 4) { // 동의 관련 선택지
-				da = survey[i].charAt(1); // 두 번째 캐릭터
-				s -= 4;
-			} else {
-				continue; // 모르겠음 선택 시 점수를 얻지 않음.
-			}
-			for (int j = 0; j < person.length; j++) {
-				if (da == person[j]) {
-					score[j] += s; // 동일한 인덱스 값에 점수를 더하기
-				}
-			}
-		}
-		
-		for (int i = 0; i < person.length; i = i + 2) {
-			if (score[i] < score[i+1]) {
-				answer += person[i+1];
-			} else if (score[i] > score[i+1]) {
-				answer += person[i];
-			} else { // 성격 유형 점수가 같으면 사전 순으로 빠른 성격 유형
-				if (person[i] > person[i+1]) {
-					answer += person[i+1];
-				} else {
-					answer += person[i];
-				}
-			}
-		}
+        // 점수 주기
+        for (int i=0; i<survey.length; i++){
+            int left = 0;
+            int right = 0;
+            int now = 0;
 
-		return answer;
-	}
+            for (int j=0; j<4; j++){
+                if (survey[i].equals(og[j])){
+                    now = j;
+                    if (choices[i] < 4){
+                        left += (4 - choices[i]);
+                    } else if (choices[i] > 4) {
+                        right += (choices[i] - 4);
+                    }
+                    break;
+                } else if (survey[i].equals(rv[j])) {
+                    now = j;
+                    if (choices[i] < 4){
+                        right += (4 - choices[i]);
+                    } else if (choices[i] > 4) {
+                        left += (choices[i] - 4);
+                    }
+                    break;
+                }
+            }
+
+            chk[now][0] += left;
+            chk[now][1] += right;
+        }
+
+        for (int i=0; i<chk.length; i++){
+            if (chk[i][0] == chk[i][1]){
+                answer += og[i].charAt(0);
+            } else if (chk[i][0] < chk[i][1]){
+                answer += og[i].charAt(1);
+            } else {
+                answer += og[i].charAt(0);
+            }
+        }
+
+
+        return answer;
+    }
 }
