975a0a3856241017e415ee787873f20c91eaa13e
sirlyun <116432941+sirlyun@users.noreply.github.com>
2025-02-18 20:18:58 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,98 +1,97 @@
 '''
-    방은 NxM 크기
-    방 안 각각의 칸은 벽 또는 빈 칸으로 구성
-    청소기는 동서남북 중 한 방향을 바라봄
-    방의 각 칸은 좌표 r, c로 표현
-    청소기 작동 방식
-        현재 칸이 청소되지 않은 경우 현재 칸 청소
-        1.현재 칸의 주변 4칸 중 청소되지 않은 빈칸이 없는경우
-            방향 유지한체 한칸 후진 가능하면 한칸 후진하고 청소 체크
-            후진 불가능하면 작동을 멈춘다
-        2.현재 칸의 주변 4칸 중 청소되지 않은 빈칸이 있는경우
-            반시계 방향으로 방향 90도 회전
-            바라보는 방향 기준으로 앞쪽 칸이 청소되지 않은 빈 칸이면 한칸 전진
-            청소 체크
+    N x M 의 방
+    각각의 칸은 벽 또는 빈 칸
+    청소기는 상하좌우 방향성이 있다 (d)
+        d = 0 (상) d = 1 (우) d = 2 (하) d = 3 (좌)
+    방의 각 칸은 (r, c) 로 나타낼 수 있다
+    좌상단이 (0, 0) 이다
+    우하단은 (N-1, M-1) 이다
+
+    동작
+        1. 현재 칸이 처음 방문하는 칸이고, 빈 칸이라면 청소
+        2. 현재 칸의 상하좌우에 청소가 안된 빈칸이 없는 경우
+            1. 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진 후 1번 동작 시행
+            2. 후진이 불가능하다면 종료
+        3. 현재 칸의 상하좌우에 청소가 안된 빈칸이 있는 경우
+            1. 반시계 방향으로 90도 회전
+            2. 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진
+            3. 1번 동작 시행
+
+    작동이 멈출 때까지 청소한 방의 개수 출력
 '''
 
-N, M = map(int, input().split())
-robot_r, robot_c, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(N)]
+import sys
 
-clean_room = 0
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-flag = False
+class Cleaner:
+    def __init__(
+            self, room_info, r, c, d, N, M
+    ):
+        self.room_info = room_info
+        self.d_info = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        self.r, self.c, self.d = r, c, d
+        self.N, self.M = N, M
+        self.exit = False
 
-while True:
-    if flag:
-        break
-    # 현재 위치 청소 체크
-    if room[robot_r][robot_c] == 0:
-        clean_room += 1
-        room[robot_r][robot_c] = 2
-    # 주변 체크
-    chk = 0
-    for k in range(4):
-        di = robot_r + dx[k]
-        dj = robot_c + dy[k]
-        if 0<=di<N and 0<=dj<M:
-            if room[di][dj] == 0:
-                chk += 1
-    # 1의 경우
-    if chk == 0:
-        if d == 0:
-            if 0<=robot_r+1<N:
-                if room[robot_r+1][robot_c] != 1:
-                    robot_r += 1
-                else:
-                    flag = True
-            else:
-                flag = True
-        elif d == 1:
-            if 0<=robot_c-1<M:
-                if room[robot_r][robot_c-1] != 1:
-                    robot_c -= 1
-                else:
-                    flag = True
-            else:
-                flag = True
-        elif d == 2:
-            if 0<=robot_r-1<N:
-                if room[robot_r-1][robot_c] != 1:
-                    robot_r -= 1
-                else:
-                    flag = True
-            else:
-                flag = True
+    def check_now(self):
+        if self.room_info[self.r][self.c] == '0':
+            self.room_info[self.r][self.c] = '2'
+
+    def check_around(self):
+        for i in range(4):
+            dr = self.r + self.d_info[i][0]
+            dc = self.c + self.d_info[i][1]
+            if 0 <= dr < self.N and 0 <= dc < self.M:
+                if self.room_info[dr][dc] == '0':
+                    return True
+
+        return False
+
+    def next_action(self, flag):
+        if flag is True:
+            for i in range(4):
+                self.d = self.d = (self.d - 1) % 4
+
+                dr = self.r + self.d_info[self.d][0]
+                dc = self.c + self.d_info[self.d][1]
+
+                if self.room_info[dr][dc] == '0':
+                    self.r = dr
+                    self.c = dc
+
+                    return
+
         else:
-            if 0<=robot_c+1<M:
-                if room[robot_r][robot_c+1] != 1:
-                    robot_c += 1
-                else:
-                    flag = True
-            else:
-                flag = True
-    # 2의 경우
-    else:
-        if d == 0:
-            d = 3
-            if 0<=robot_c-1<M:
-                if room[robot_r][robot_c-1] == 0:
-                    robot_c -= 1
-        elif d == 1:
-            d = 0
-            if 0<=robot_r-1<N:
-                if room[robot_r-1][robot_c] == 0:
-                    robot_r -= 1
-        elif d == 2:
-            d = 1
-            if 0<=robot_c+1<M:
-                if room[robot_r][robot_c+1] == 0:
-                    robot_c += 1
-        else:
-            d = 2
-            if 0<=robot_r+1<N:
-                if room[robot_r+1][robot_c] == 0:
-                    robot_r += 1
+            d = (self.d + 2) % 4
 
-print(clean_room)
+            dr = self.r + self.d_info[d][0]
+            dc = self.c + self.d_info[d][1]
+
+            if 0 <= dr < self.N and 0 <= dc < self.M:
+                if self.room_info[dr][dc] != '1':
+                    self.r = dr
+                    self.c = dc
+
+                    return
+
+            self.exit = True
+            return
+
+
+N, M = map(int, sys.stdin.readline().split())
+r, c, d = map(int, sys.stdin.readline().split())
+room_info = [sys.stdin.readline().split() for _ in range(N)]
+
+cleaner = Cleaner(room_info=room_info, r=r, c=c, d=d, N=N, M=M)
+
+while not cleaner.exit:
+    cleaner.check_now()
+    flag = cleaner.check_around()
+    cleaner.next_action(flag=flag)
+
+count = 0
+for n in range(N):
+    for m in range(M):
+        if cleaner.room_info[n][m] == '2':
+            count += 1
+
+print(count)
